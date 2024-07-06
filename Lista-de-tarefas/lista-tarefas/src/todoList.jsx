ca07473ad69c48a51a49417a6192a5e29ffdbf1b
Leonardo Teixeira Martins <leonardo.t@escolar.ifrn.edu.br>
2024-07-06 11:49:24 -0300

    parte visual criada.

--- Lista-de-tarefas/lista-tarefas/src/todoList.jsx
+++ Lista-de-tarefas/lista-tarefas/src/todoList.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import "./todoList.css";
 
 function TodoList() {
   return (
     <div>
       <h1>Lista de tarefas</h1>
+      <form action="">
+        <input 
+        type="text"
+        placeholder="Digite uma tarefa" 
+        />
+        <button className="add" type="submit">add</button>
+      </form>
+      <div className="listaTarefas">
+        <div className="item">
+            <span>Tarefa de exemplo</span>
+            <button className="del">Deletar</button>
+        </div>
+        <div className="item-completo">
+            <span>Tarefa de exemplo</span>
+            <button className="del">Deletar</button>
+        </div>
+        <button className="deleteAll">Deletar Todas</button>
+      </div>
     </div>
   );
 }
